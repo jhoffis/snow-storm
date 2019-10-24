abb68624aa38e6ff2608d4eac3940332411cf86c
S571736 <Sondre.Gjesdal@gmail.com>
2019-10-24 15:04:32 +0200

    snow added

---
+++ js/terrain/trees.js
@@ -0,0 +1,33 @@
+"use strict";
+
+import {BoxGeometry, MeshBasicMaterial, Points, Vector3} from "../lib/three.module.js";
+import TerrainBufferGeometry from "./TerrainBufferGeometry.js";
+
+export default class Trees {
+    constructor(terrain, terrainWidth, scene) {
+        //creating tree variable, with 100 trees
+        let treeCount = 100,
+            treeGeometry = new BoxGeometry(1, 3, 1),
+            treeMaterial = new MeshBasicMaterial({color: 0xff0000});
+
+        //creating each individual tree
+        for (let i = 0; i < treeCount; i++) {
+            let pX = Math.random() * terrainWidth - terrainWidth/2,
+                pZ = Math.random() * terrainWidth - terrainWidth/2,
+                pY = terrain.getHeightAtPrecise(new Vector3(pX, 0, pZ)),
+                tree = new Vector3(pX, pY, pZ);
+
+            //pushing trees to the geometry
+            treeGeometry.vertices.push(tree);
+        }
+
+        let treeSystem = new Points(
+            treeGeometry,
+            treeMaterial
+        );
+
+        scene.add(treeSystem);
+    }
+
+
+}
