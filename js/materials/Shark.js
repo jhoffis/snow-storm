65202f2c89cf476113d358b435c217453a18a164
571733 <jindrebo@hotmail.no>
2019-11-14 13:09:26 +0100

    sharkAgain

--- js/materials/Shark.js
+++ js/materials/Shark.js
@@ -6,14 +6,14 @@ class Shark{
         let parent = this;
         this.object = new ExternalObject(scene, 'res/models/shark/scene.gltf', 1, 1, 1, 1,
             function s() {
-                parent.mixer = new THREE.AnimationMixer(object.gltf.scene);
+                parent.mixer = new THREE.AnimationMixer(parent.object.gltf.scene);
                 parent.object.gltf.animations.forEach((clip) => {
                     let clipAction = parent.mixer.clipAction(clip)
                     clipAction.play();
                 });
             }
         );
-        
+
         //var delta = clock.getDelta();
         // mixer.update( delta )
 
