b7fda435fde31532be41f8a2a348951de37cbaf5
jhoffis <jonahoffis@gmail.com>
2019-11-18 12:14:59 +0100

    shader gjort til webgl 2

--- js/materials/BestMaterial.js
+++ js/materials/BestMaterial.js
@@ -3,7 +3,9 @@ class BestMaterial extends THREE.ShaderMaterial {
         super();
 
         const vertexShader = `
-        varying vec2 vUv;                                                           // Export texture coordinates so we get them in the fragment shader.
+        #version 300 es
+        
+        out vec2 vUv;                                                           // Export texture coordinates so we get them in the fragment shader.
 		void main() {
             vUv = uv;                                                                       // Threejs gives us the texture coordinates in the 'uv' variable, so set that to our vUv variable. 
 			gl_Position	= projectionMatrix * modelViewMatrix * vec4(position, 1.0);         // Translate position to screen coordinates and set the gl_Position
@@ -11,15 +13,18 @@ class BestMaterial extends THREE.ShaderMaterial {
 	`;
 
         const fragmentShader = `
-        //uniform sampler2D texture;                                                         // Uniform controlling the texture
+        #version 300 es
+
+		precision mediump float;
         uniform vec3 colorMask;                                                            // Uniform controlling the colorMask
-       // uniform vec4 color = vec4(fract(sin(colorMask.x)*1.0), fract(sin(colorMask.y)*1.0), fract(sin(colorMask.z)*1.0), 1.0);
         
-        varying vec2 vUv;                                                                  // Receive the texture coordinates from our vertex shader
+        in vec2 vUv;                                                                  // Receive the texture coordinates from our vertex shader
+        out vec4 fColor;
 
 		void main() {
-            //vec4 textureColor = texture2D(texture, vUv);                                   // Sample the texture using the coordinates      
-			gl_FragColor = vec4(colorMask, 1.0);                        // Set the fragment color, applying the colorMask to the texture color.
+            // vec4 textureColor = texture2D(texture, vUv);                                   // Sample the texture using the coordinates      
+			// gl_FragColor = vec4(colorMask, 1.0);                        // Set the fragment color, applying the colorMask to the texture color.
+            fColor = vec4(colorMask, 1.0);;
 		}
 
 	`;
