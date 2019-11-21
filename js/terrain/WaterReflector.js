33b4a40fc935871844986e8b7b0ac2445f753588
S571736 <Sondre.Gjesdal@gmail.com>
2019-11-21 13:16:12 +0100

    endra vannstørrelsen

--- js/terrain/WaterReflector.js
+++ js/terrain/WaterReflector.js
@@ -6,10 +6,10 @@ class WaterReflector {
     constructor(terrainWidth, scene) {
         this.waterHeight = 10;
         let waterSize = 2000;
-        let waterGeometry = new THREE.PlaneBufferGeometry(terrainWidth, terrainWidth);
+        let waterGeometry = new THREE.PlaneBufferGeometry(terrainWidth*2, terrainWidth*2);
         let water = new Water_S(waterGeometry, {
             color: 0x9DF2DB, //9DF2DB og ffffff
-            scale: 4,
+            scale: 8,
             textureWidth: 1024,
             textureHeight: 1024,
             flowSpeed: 0.01
